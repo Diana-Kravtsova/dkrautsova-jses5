ea63fe041d62e3f372814b8efc67daacd819eb49
DKrautsova <DKrautsova@gomel.iba.by>
2022-07-12 15:59:54 +0300

    fix Task 4

--- src/task4/task4.js
+++ src/task4/task4.js
@@ -1,16 +1,7 @@
 let countBs = (string) => {
-	let count = 0;
-	for (let i = 0; i < string.length - 1; i++) {
-		if(string[i] === 'B'){
-			count++;
-		}
-	}
-	return count;
-}
-/*
-let countBs = (string) => {
 	return countChar(string, 'B');
-}*/
+}
+
 let countChar = (string, symbol) => {
 	let count = 0;
 	for (const element of string) {
@@ -20,13 +11,3 @@ let countChar = (string, symbol) => {
 	}
 	return count;
 }
-/*
-let countChar = (string, symbol) => {
-	let count = 0;
-	for (let i = 0; i < string.length; i++) {
-		if(string[i] === symbol){
-			count++;
-		}
-	}
-	return count;
-}*/
