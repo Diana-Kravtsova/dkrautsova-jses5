4b70f1a03d84bf4c78601224465147b12deaacef
DKrautsova <DKrautsova@gomel.iba.by>
2022-11-03 00:07:08 +0300

    add Task 39

---
+++ src/task39/server.js
@@ -0,0 +1,155 @@
+import url from "url";
+import http from "http";
+import fs from "fs";
+import mime from "mime";
+
+function startServer() {
+	let methods = {};
+
+	http.createServer(function (request, response) {
+		function respond(code, body, type) {
+			if (!type)
+				type = "text/plain";
+
+			response.writeHead(code, {"Content-Type": type});
+
+			if (body && body.pipe)
+				body.pipe(response);
+			else
+				response.end(body);
+		}
+
+		if (request.method in methods)
+			methods[request.method](urlToPath(request.url),
+				respond, request);
+		else
+			respond(405, "Method " + request.method + " not allowed.");
+	}).listen(8000);
+
+	methods.GET = get;
+	methods.PUT = put;
+	methods.DELETE = remove;
+	methods.MKCOL = mkcol;
+}
+
+
+function get(path, respond, request) {
+	// Get header type
+	let type = request.headers.type;
+
+	// Exception for index.html, main.js and style.css
+	if (!type) {
+		if (path === './main.js' || path === './style.css' || path === './')
+			type = 'file';
+		if (path === './')
+			path += 'index.html';
+	}
+
+	// Check type
+	switch (type) {
+		case 'file':
+			// Check and read the file
+			fs.stat(path, function (error) {
+				if (error && error.code === 'ENOENT') {
+					respond(404, 'Not found');
+				} else {
+					checkError(error, respond, function () {
+						respond(200, fs.createReadStream(path),
+							mime.getType(path));
+					});
+				}
+			});
+			break;
+		case 'path':
+			// Check and read the path
+			fs.readdir(path, {withFileTypes: true}, function (error, files) {
+				checkError(error, respond, function () {
+					let list = [];
+					files.forEach(function (elem) {
+						if (elem.isDirectory())
+							list.push(elem.name + '/');
+						else
+							list.push(elem.name);
+					});
+
+					respond(200, JSON.stringify(list));
+				});
+			});
+			break;
+		default:
+			respond(403, 'Forbidden');
+			break;
+	}
+}
+
+function put(path, respond, request) {
+	let stream = fs.createWriteStream(path);
+
+	stream.on('error', function (error) {
+		respond(500, error.toString());
+	});
+
+	stream.on('finish', function () {
+		respond(204);
+	});
+
+	request.pipe(stream);
+}
+
+function mkcol(path, respond) {
+	// Check a given path
+	fs.stat(path, function (error, stats) {
+		if (error && error.code === "ENOENT")
+			fs.mkdir(path, function (err) {
+				checkError(err, respond, function () {
+					respond(200, 'The directory ' +
+						path + ' is successfully created');
+				});
+			});
+		else if (stats.isDirectory())
+			respond(204);
+		else
+			respond(400);
+	});
+}
+
+function remove(path, respond) {
+	fs.stat(path, function (error, stats) {
+		if (error && error.code === 'ENOENT')
+			respond(204);
+
+		else if (stats.isDirectory())
+			// Check and remove path
+			fs.rmdir(path, function (err) {
+				checkError(err, respond, function () {
+					respond(200, 'The directory ' +
+						path + ' is successfully removed');
+				});
+			});
+
+		else
+			// Check and remove file
+			fs.unlink(path, function (err) {
+				checkError(err, respond, function () {
+					respond(200, 'The file ' + path +
+						' is successfully removed');
+				});
+			});
+	});
+}
+
+function urlToPath(urlStr) {
+	let path = url.parse(urlStr).pathname;
+	path = path.replace(/[\/\\]\.\./g, '');
+	return "." + decodeURIComponent(path);
+}
+
+function checkError(error, respond, callback) {
+	if (error) {
+		respond(500, error.toString());
+	} else {
+		callback();
+	}
+}
+
+startServer();
---
+++ src/task39/task39.js
@@ -0,0 +1,200 @@
+function start() {
+	// Create form from objects
+	let form = {
+		path: document.getElementById('path'),
+		file: document.getElementById('file'),
+		select: document.getElementById('select'),
+		back: document.getElementById('back'),
+		createFile: document.getElementById('createFile'),
+		inputFile: document.getElementById('inputFile'),
+		createDir: document.getElementById('createDir'),
+		inputDir: document.getElementById('inputDir'),
+		deleteFile: document.getElementById('deleteFile'),
+		deleteDir: document.getElementById('deleteDir'),
+		save: document.getElementById('save'),
+		text: document.getElementById('text'),
+		pathname: '/',
+		filename: ''
+	};
+
+	displayPath(form, getPath('/'));
+
+	// Add all form event listeners
+	addEvents(form);
+}
+
+function addEvents(form) {
+	form.select.addEventListener('change', function() {
+		let options = form.select.childNodes;
+		let selected = '';
+
+		// Looking for selected option
+		for (const element of options) {
+			if (element.selected) {
+				selected = element.text;
+				break;
+			}
+		}
+
+		// Check is it a directory or a file
+		if (/\//.test(selected)) {
+			form.pathname += selected;
+			displayPath(form, getPath(form.pathname));
+			form.back.disabled = false;
+			form.deleteDir.disabled = false;
+			closeFile(form);
+		} else {
+			form.filename = selected;
+			displayFile(form, loadFile(form.pathname + form.filename));
+			form.save.disabled = false;
+			form.deleteFile.disabled = false;
+		}
+	});
+
+	// Click a 'back' button
+	form.back.addEventListener('click', function(event) {
+		form.pathname = form.pathname.slice(0, form.pathname.slice(0,
+			form.pathname.length - 1).lastIndexOf('/') + 1);
+		displayPath(form, getPath(form.pathname));
+
+		closeFile(form);
+
+		// Check root directory
+		if (form.pathname === '/') {
+			form.back.disabled = true;
+			form.deleteDir.disabled = true;
+		}
+	});
+
+	// Click a 'Create file' button
+	form.createFile.addEventListener('click', function() {
+		saveFile(form.pathname + form.inputFile.value, '');
+		displayPath(form, getPath(form.pathname));
+		form.inputFile.value = '';
+		form.inputFile.dispatchEvent(new Event('input'));
+	});
+
+	form.inputFile.addEventListener('input', function() {
+		form.createFile.disabled = form.inputFile.value === '';
+	});
+
+	// Click a 'Create directory' button
+	form.createDir.addEventListener('click', function() {
+		createDirectory(form.pathname + form.inputDir.value);
+		displayPath(form, getPath(form.pathname));
+		form.inputDir.value = '';
+		form.inputDir.dispatchEvent(new Event('input'));
+	});
+
+	form.inputDir.addEventListener('input', function() {
+		form.createDir.disabled = form.inputDir.value === '';
+	});
+
+	// Click a 'Delete file' button
+	form.deleteFile.addEventListener('click', function() {
+		remove(form.pathname + form.filename);
+		displayPath(form, getPath(form.pathname));
+		closeFile(form);
+	});
+
+	// Click a 'Delete directory' button
+	form.deleteDir.addEventListener('click', function() {
+		remove(form.pathname);
+		form.pathname = form.pathname.slice(0, form.pathname.slice(0,
+			form.pathname.length - 1).lastIndexOf('/') + 1);
+		displayPath(form, getPath(form.pathname));
+		if (form.pathname === '/') {
+			form.back.disabled = true;
+			form.deleteDir.disabled = true;
+		}
+	});
+
+	// Click a 'Save' button
+	form.save.addEventListener('click', function() {
+		// Save file
+		saveFile(form.pathname + form.filename, form.text.value);
+	});
+}
+
+function getPath(path) {
+	// Request to get the path
+	return JSON.parse(urlRequest('GET', path, null, 'path'));
+}
+
+function loadFile(filename) {
+	// Request to load the file
+	return urlRequest('GET', filename, null, 'file');
+}
+
+function saveFile(filename, text) {
+	// Request to save the file
+	return urlRequest('PUT', filename, text);
+}
+
+function createDirectory(path) {
+	// Request to create the directory
+	return urlRequest('MKCOL', path, null);
+}
+
+function remove(path) {
+	// Request to delete the directory or the file
+	return urlRequest('DELETE', path, null);
+}
+
+function displayFile(form, text) {
+	form.text.value = text;
+	form.file.textContent = form.filename;
+}
+
+function closeFile(form){
+	form.filename = '';
+	form.file.textContent = '';
+	form.text.value = '';
+	form.deleteFile.disabled = true;
+	form.save.disabled = true;
+}
+
+function displayPath(form, list) {
+	let options = form.select.childNodes;
+
+	// Remove all old options
+	for (let i = 0; i < options.length;) {
+		form.select.removeChild(options[i]);
+	}
+
+	// Create new options
+	list.forEach(function(elem) {
+		let option = document.createElement('option');
+		option.appendChild(document.createTextNode(elem));
+		form.select.appendChild(option);
+	});
+
+	// Condition for no popup list
+	if (options.length >= 2) {
+		form.select.size = options.length;
+	} else {
+		form.select.size = 2;
+	}
+
+	form.path.textContent = form.pathname;
+}
+
+function urlRequest(method, url, body, type) {
+	let req = new XMLHttpRequest();
+
+	// Configuring of request
+	req.open(method, url, false);
+
+	// Set header type
+	req.setRequestHeader('type', type);
+
+	// Set body request
+	req.send(body);
+
+	// Return request result
+	if (req.status < 400) {
+		return req.responseText;
+	} else {
+		return 'Request failed: ' + req.status + '\n' + req.responseText;
+	}
+}
